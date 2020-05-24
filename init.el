75644e7639e0b3dbbd4a880560fa17fc3ca304b0
Matt Erickson <matterickson9819@gmail.com>
2020-05-24 14:59:23 -0500

    Added init elisp file

---
+++ init.el
@@ -0,0 +1,42 @@
+;; Matt's emacs init file
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '(("melpa" . "https://melpa.org/packages/")
+	       ("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+
+;; Makes sure these packages are installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+
+;; Load configurations stored in org mode
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "SRC" :family "Hack")))))
+ 
